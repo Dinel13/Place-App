cc4b44cc5faf4ee142a850ce18feb14ac45721e2
dinel <solahuddin.hafid.tomaballo@gmail.com>
2020-11-11 22:24:39 +0800

    using filesystem devices

--- store/PlaceAction.js
+++ store/PlaceAction.js
@@ -1,5 +1,23 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, imageUri: image } };
+  return (dispatch) => {
+    const fileName = image.split("/").pop(); //file name
+
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      FileSystem.moveAsync({
+        form: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, imageUri: newPath } });
+  };
 };
