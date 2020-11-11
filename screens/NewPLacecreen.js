e2281567b18453150707b98d6544b97b8dff8212
dinel <solahuddin.hafid.tomaballo@gmail.com>
2020-11-11 19:47:37 +0800

    basic state di placescreen

--- screens/NewPLacecreen.js
+++ screens/NewPLacecreen.js
@@ -1,14 +1,56 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React , {useState}from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
+import Colors from "../constants/Colors";
 
 const NewPLacecreen = (props) => {
+  const [titleValue, settitleValue] = useState("")
+
+  const titleChangeHandler = text => {
+    settitleValue(text)
+  }
+  
+  const saveHandler = () => {
+
+  }
+
   return (
-    <View>
-      <Text>buat lokasi</Text>
-    </View>
+    <ScrollView>
+      <View style={style.form}>
+        <Text style={style.label}>Title</Text>
+        <TextInput style={style.textINput} onChangeText={titleChangeHandler} value={titleValue} />
+        <Button title="Save Place" color={Colors.primary} onPress={saveHandler} />
+      </View>
+    </ScrollView>
   );
 };
 
-const style = StyleSheet.create({});
+NewPLacecreen.navigationOptions = {
+  headerTitle : 'Tambah Lokasi'
+}
+
+const style = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textINput : {
+    borderBottomColor : '#ccc',
+    borderBottomWidth : 1,
+    marginBottom : 15,
+    paddingVertical : 4,
+    paddingHorizontal: 2
+
+  }
+});
 
 export default NewPLacecreen;
